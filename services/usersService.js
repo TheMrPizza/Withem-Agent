5cc2eb38ac63ed1b0008ead7717348b1ef84d4a4
Guy Cohen <guycohen08@gmail.com>
2022-09-05 10:14:54 +0300

    Connected to db

--- services/usersService.js
+++ services/usersService.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
+import 'react-native-get-random-values';
+import { v4 } from 'uuid';
 
 const client = axios.create({
-	baseURL: '',
-	timeout: 5000
+	baseURL: 'https://39eb49ae41f748febb53d209417b9092.francecentral.azure.elastic-cloud.com/search-users/_doc',
+	timeout: 5000,
+	headers: {
+		Authorization: 'ApiKey ZzV4eUNJTUJ2SWZVOVVlV0JrMGs6VFpRSkxMWnVTYXE3SE4wUXBRenpCZw=='
+	}
 });
 
 const createUser = async ({firstName, lastName, contacts}) => {
 	const response = await client.post('/', {
-		firstName,
-		lastName,
-		contacts
+		id: v4(),
+		name: `${firstName} ${lastName}`,
+		contacts: contacts.map(({name, phoneNumber}) => ({
+			[name]: phoneNumber
+		})),
+		phone: v4()
 	});
+
+	console.log(response);
 
 	return response;
 };
